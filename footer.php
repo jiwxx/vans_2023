c8cd475698e19f6b53cd84b4818996fd2998fc21
jiwoo <wldnslzhf@gmail.com>
2023-08-24 18:13:17 +0900

    상품 리스트

---
+++ footer.php
@@ -0,0 +1,47 @@
+<footer>
+        <div class="top">
+            <div class="top-left">
+                <ul>
+                    <li>
+                        <a href="#">새로운 소식</a>
+                        <a href="#">매장안내</a>
+                        <a href="#">가입 확인</a>
+                    </li>
+                    <li>
+                        <a href="#">ABOUT VANS</a>
+                        <a href="#">반스에 대하여</a>
+                        <a href="#">고객센터</a>
+                    </li>
+                    <li><a href="#">도움말</a>
+                        <ul class="snb">
+                            <li><a href="#">로그인 안내</a></li>
+                            <li><a href="#">주문배송 조회</a></li>
+                            <li><a href="#">반품 정책</a></li>
+                            <li><a href="#">결제 방법</a></li>
+                            <li><a href="#">공지사항</a></li>
+                            <li><a href="#">문의하기</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            <div class="top-right">
+                <a href="#">페이스북</a>
+                <a href="#">트위터</a>
+                <a href="#">유튜브</a>
+                <a href="#">인스타그램</a>
+            </div>
+        </div>
+        <div class="middle-top">
+            <p><img src="./images/icon_location.png" alt="">서울특별시 강남구 테헤란로 317 동훈타워 18층 | 개인정보 책임자 남미리내</p>
+            <div class="top-right">
+                <p>이용약관</p>
+                <p>개인정보처리방침</p>
+                <p>청소년 보호정책</p>
+            </div>
+        </div>
+        <div class="btm">
+            <p>유한책임회사 브이에프코리아 | 대표 TZE CHOI THEODORE PANG | 등록번호 220-88-43561 | 통신판매업신고번호 2013-서울강남-02918호 <a href="#">사업자정보확인</a> <br>
+            서울특별시 강남구 테헤란로 317 동훈타워 18층 | 개인정보 책임자 남미리내 | 고객센터(평일 09:00 ~ 18:00) 1522-1882 | 이메일<a href="#">Vanskr_online@vfc.com</a> <br>
+            &copy;유한책임회사 브이에프코리아. All Rights Reserved. California Transparency in Supply Chain Act of 2010 <br></p>
+        </div>
+    </footer>
