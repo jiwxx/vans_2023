1007410e9c8a20afb4aba23c6b8ed0f512bae71e
jiwoo <wldnslzhf@gmail.com>
2023-08-28 10:35:39 +0900

    php까지

--- footer.php
+++ footer.php
@@ -33,15 +33,15 @@
         </div>
         <div class="middle-top">
             <p><img src="./images/icon_location.png" alt="">서울특별시 강남구 테헤란로 317 동훈타워 18층 | 개인정보 책임자 남미리내</p>
-            <div class="top-right">
-                <p>이용약관</p>
-                <p>개인정보처리방침</p>
-                <p>청소년 보호정책</p>
+            <div class="middle-top-right">
+                <a href="#">이용약관</a>
+                <a href="#">개인정보처리방침</a>
+                <a href="#">청소년 보호정책</a>
             </div>
         </div>
-        <div class="btm">
+        <address>
             <p>유한책임회사 브이에프코리아 | 대표 TZE CHOI THEODORE PANG | 등록번호 220-88-43561 | 통신판매업신고번호 2013-서울강남-02918호 <a href="#">사업자정보확인</a> <br>
             서울특별시 강남구 테헤란로 317 동훈타워 18층 | 개인정보 책임자 남미리내 | 고객센터(평일 09:00 ~ 18:00) 1522-1882 | 이메일<a href="#">Vanskr_online@vfc.com</a> <br>
             &copy;유한책임회사 브이에프코리아. All Rights Reserved. California Transparency in Supply Chain Act of 2010 <br></p>
-        </div>
+        </address>
     </footer>
