30635e2a7b1fda8f0dbd29b3ed880248df69fd28
jiwoo <wldnslzhf@gmail.com>
2023-08-29 18:11:55 +0900

    js 메뉴

---
+++ script/header.js
@@ -0,0 +1,49 @@
+const gnb = document.querySelectorAll('.gnb')
+const nav = document.querySelector('.snb_all')
+const nav_bg = document.querySelector('.nav_bg')
+console.log(gnb,nav,nav_bg)
+
+gnb[0].addEventListener('mouseover',function(){
+    nav.style.display ='flex'
+    nav_bg.style.display ='flex'
+})
+gnb[0].addEventListener('mouseout',function(){
+    nav.style.display ='none' 
+    nav_bg.style.display ='none'
+})
+
+gnb[1].addEventListener('mouseover',function(){
+    nav.style.display ='flex'
+    nav_bg.style.display ='flex'
+})
+gnb[1].addEventListener('mouseout',function(){
+    nav.style.display ='none' 
+    nav_bg.style.display ='none'
+})
+
+gnb[2].addEventListener('mouseover',function(){
+    nav.style.display ='flex'
+    nav_bg.style.display ='flex'
+})
+gnb[2].addEventListener('mouseout',function(){
+    nav.style.display ='none' 
+    nav_bg.style.display ='none'
+})
+
+gnb[3].addEventListener('mouseover',function(){
+    nav.style.display ='flex'
+    nav_bg.style.display ='flex'
+})
+gnb[3].addEventListener('mouseout',function(){
+    nav.style.display ='none' 
+    nav_bg.style.display ='none'
+})
+
+gnb[5].addEventListener('mouseover',function(){
+    nav.style.display ='flex'
+    nav_bg.style.display ='flex'
+})
+gnb[5].addEventListener('mouseout',function(){
+    nav.style.display ='none' 
+    nav_bg.style.display ='none'
+})
