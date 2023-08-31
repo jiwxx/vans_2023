036ea10e2457f81cc78ff0fdc4608b4756f9e919
jiwoo <wldnslzhf@gmail.com>
2023-08-31 17:41:17 +0900

    마무리/파비콘 넣어야됨

--- script/shoes.js
+++ script/shoes.js
@@ -1,13 +1,17 @@
-const title = document.querySelectorAll ('right > ul > li > h4')
+const title = document.querySelectorAll ('.right > ul > li > h4')
 const txt = document.querySelectorAll('.txt_contents > .txt')
 console.log(txt,title)
 
 for(let i of txt){i.style.display='none'}
-for(let i of title){
-    console.log(i)
-    i.addEventListener('click',function(){
-        txt[0].classList.toggle('.active')
-        txt[1].classList.toggle('.active')
-        txt[2].classList.toggle('.active')
+// for(let i of title){
+//     i.addEventListener('click',function(){
+//         txt[0].classList.toggle('active')
+//     })
+// }
+title.forEach(function(t,i){
+    t.addEventListener('click',function(){
+        for(let j of txt){j.classList.remove('active')}
+        //console.log(t,i,txt[i])
+        txt[i].classList.toggle('active')
     })
-}
+})
